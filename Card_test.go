f17cfa9b61b5df987c69fa599f6722e50be3f837
Gabriel  Cowan <gabrielcowan@Gabriels-MacBook-Pro.local>
2021-01-28 16:00:47 +0000

    first commit

---
+++ Card_test.go
@@ -0,0 +1,95 @@
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+// Agent is ...
+func ExampleCard() {
+	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Three, Suit: Diamond})
+	fmt.Println(Card{Rank: Four, Suit: Heart})
+	fmt.Println(Card{Rank: King, Suit: Spade})
+	fmt.Println(Card{Rank: Six, Suit: Heart})
+	fmt.Println(Card{Rank: Jack, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
+
+	//Output:
+	//Ace of Hearts
+	//Three of Diamonds
+	//Four of Hearts
+	//King of Spades
+	//Six of Hearts
+	//Jack of Clubs
+	//Joker
+
+}
+func TestNew(t *testing.T) {
+	cards := New()
+	// 13 * 4 =
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards in a new deck")
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	// make shuffleRand deterministic
+	// First call to shuffleRand.Perm(52) should be:
+	// [40 35 ...]
+	shuffleRand = rand.New(rand.NewSource(0))
+
+	orig := New()
+	first := orig[40]
+	second := orig[35]
+	cards := New(Shuffle)
+	if cards[0] != first {
+		t.Errorf("Expected the first card to be %s, received %s.", first, cards[0])
+	}
+
+	if cards[1] != second {
+		t.Errorf("Expected the first card to be %s, received %s.", second, cards[1])
+	}
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count++
+		}
+	}
+	if count != 3 {
+		t.Error("Expected 3 Jokers, received:", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func(card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("expected all twos and threes to be filtered out.")
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	if len(cards) != 13*4*3 {
+		t.Errorf("Expected %d cards, received %d cards.", 13*4*3, len(cards))
+	}
+}
